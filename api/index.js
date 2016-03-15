050232ce1a16ac441c201d7679336a8ea5e79b06
Koen Houtman <koenhoutman1991@me.com>
2016-03-15 19:10:11 +0100

    Deletion of the animate to educate branch - cleaned up a few bits of code - added some comments where I missed them

--- api/index.js
+++ api/index.js
@@ -2,16 +2,28 @@ var fs 		= require('fs'),
 	express = require('express'),
 	router 	= express.Router();
 
+/*
+ * general request handler to all routes on /api
+ * 
+ */
 router.use(function(req, res, next) {
 	res.set('Content-Type', 'application/json');
 	res.status(200);
 	next();
 });
 
+/*
+ * /api
+ * returns a nice empty hash with only success
+ */
 router.get('/', function(req, res) {
 	res.send({success: true});
 });
 
+/*
+ * /api/files
+ * returns all files listed in assets/sounds
+ */
 router.get('/files', function(req, res) {
 
 	var files = fs.readdir('./assets/sounds', function(err, data) {
@@ -20,7 +32,7 @@ router.get('/files', function(req, res) {
 			res.status(500);
 			res.send({
 				status: 'error'
-			})
+			});
 		} else {
 			res.send(data);
 		}
