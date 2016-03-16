b72db897ca37e8a7f98b7741632f8baa03377452
Koen Houtman <koenhoutman1991@me.com>
2016-03-16 14:00:10 +0100

    create a small utility for retrieving bower loaded assets - makes use bower.json to locate all files - this should make including bower assets a breeze by just installing bower components and they get injected automagically

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var ejs 	= require('ejs');
+var fs 		= require('fs');
 var app 	= express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/bower_components'));
+
+console.log(require('./lib/map-bower-files'));
 
 app.get('/', function(req, res) { 
 	res.render('app'); 
