ab7d2ee076dd23e8e01f9a16cbe809ef1e7b79a9
Koen Houtman <koenhoutman1991@me.com>
2016-03-17 07:59:01 +0100

    rounded up bower hook - added script that searches for the bower files - bower files are passed to the template - added ejs layouts

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var ejs 	= require('ejs');
+var layouts = require('express-ejs-layouts');
 var fs 		= require('fs');
 var app 	= express();
 
+
 app.set('view engine', 'ejs');
+app.set('layout', 'layout');
+app.use(layouts);
+
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(__dirname + '/bower_components'));
 
-console.log(require('./lib/map-bower-files'));
+/*
+ *	stores app-specific information
+ *	- is also the template variable scope
+ */
+var local = {};
 
+/*
+ *	retrieves bower component assets and stores them local
+ */
+local.bower = require('./lib/map-bower-files');
+
+/*
+ * request handler for each request
+ * - appends local scope to the response object
+ */
+app.use(function(req, res, next) {
+	res.locals = local;
+	next();
+});
+
+/*
+ * index route, renders the application view
+ * - appends local scope to the response object
+ */
 app.get('/', function(req, res) { 
 	res.render('app'); 
 });
